14512b3d8e4980104420b3c072d8e46beb253ba1
Sergey Titkov <Srg.titkov@gmail.com>
2025-01-30 12:07:32 +0300

    Обновил заголовок

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🗃 Ящик",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
