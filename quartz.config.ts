2802e43bdf42341c370d245089da0aeea5d0f1cd
Sergey Titkov <Srg.titkov@gmail.com>
2025-03-26 16:23:57 +0300

    vault backup: 2025-03-26 16:23:57

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "sergey-titkov.github.io/mynoutes/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
